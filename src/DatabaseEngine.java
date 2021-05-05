caac572dbcbe71cf3c82684371eb4b7edd95ec73
Ruoshi Li <44337694+ruoshi93@users.noreply.github.com>
2021-05-05 16:26:57 +0200

    Comment out the code that is not used temporarily

--- src/DatabaseEngine.java
+++ src/DatabaseEngine.java
@@ -1,83 +1,83 @@
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+//import java.io.FileNotFoundException;
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
+//import java.util.HashMap;
+//import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvValidationException;
+//import java.util.Map;
+//
+//import com.opencsv.CSVReader;
+//import com.opencsv.exceptions.CsvValidationException;
 
 public class DatabaseEngine {
 
-	/**
-	 * This method converts an CSV File to HashMap.
-	 * 
-	 * @param path The path of the CSV File.
-	 * @param key  The index of the column, which is the key of the HashMap.
-	 * @return The converted HashMap.
-	 */
-	public static Map<String, List<String>> csvToHashMap(String path, int key) {
-
-		Map<String, List<String>> values = new MyHashMap<String, List<String>>();
-
-		try {
-
-			CSVReader reader = new CSVReader(new FileReader(path));
-
-			String[] nextLine;
-
-			while ((nextLine = reader.readNext()) != null) {
-				List<String> list = new ArrayList<String>();
-				int i = 0;
-				while (i < nextLine.length) {
-					list.add(nextLine[i]);
-					i++;
-				}
-				values.put(nextLine[key], list);
-			}
-
-		} catch (CsvValidationException e) {
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		return values;
-	}
-
-	/**
-	 * This method joins two HashMaps.
-	 * 
-	 * @param m1  The first HashMap.
-	 * @param m2  The second HashMap.
-	 * @param key The index of the key in the joined HashMap.
-	 * @return The joined HashMap.
-	 */
-	public static Map<String, List<String>> join(Map<String, List<String>> m1, Map<String, List<String>> m2, int key) {
-		Map<String, List<String>> m = new MyHashMap<String, List<String>>();
-
-		Iterator<Map.Entry<String, List<String>>> it1 = m1.entrySet().iterator();
-
-		while (it1.hasNext()) {
-			HashMap.Entry<String, List<String>> entry1 = it1.next();
-			String s1 = entry1.getKey();
-			List<String> list1 = entry1.getValue();
-			List<String> list2 = m2.get(s1);
-			if (list2 != null) {
-				list1.addAll(list2);
-				m.put(list1.get(key), list1);
-			}
-		}
-
-		return m;
-	}
+//	/**
+//	 * This method converts an CSV File to HashMap.
+//	 * 
+//	 * @param path The path of the CSV File.
+//	 * @param key  The index of the column, which is the key of the HashMap.
+//	 * @return The converted HashMap.
+//	 */
+//	public static Map<String, List<String>> csvToHashMap(String path, int key) {
+//
+//		Map<String, List<String>> values = new MyHashMap<String, List<String>>();
+//
+//		try {
+//
+//			CSVReader reader = new CSVReader(new FileReader(path));
+//
+//			String[] nextLine;
+//
+//			while ((nextLine = reader.readNext()) != null) {
+//				List<String> list = new ArrayList<String>();
+//				int i = 0;
+//				while (i < nextLine.length) {
+//					list.add(nextLine[i]);
+//					i++;
+//				}
+//				values.put(nextLine[key], list);
+//			}
+//
+//		} catch (CsvValidationException e) {
+//			e.printStackTrace();
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//
+//		return values;
+//	}
+//
+//	/**
+//	 * This method joins two HashMaps.
+//	 * 
+//	 * @param m1  The first HashMap.
+//	 * @param m2  The second HashMap.
+//	 * @param key The index of the key in the joined HashMap.
+//	 * @return The joined HashMap.
+//	 */
+//	public static Map<String, List<String>> join(Map<String, List<String>> m1, Map<String, List<String>> m2, int key) {
+//		Map<String, List<String>> m = new MyHashMap<String, List<String>>();
+//
+//		Iterator<Map.Entry<String, List<String>>> it1 = m1.entrySet().iterator();
+//
+//		while (it1.hasNext()) {
+//			HashMap.Entry<String, List<String>> entry1 = it1.next();
+//			String s1 = entry1.getKey();
+//			List<String> list1 = entry1.getValue();
+//			List<String> list2 = m2.get(s1);
+//			if (list2 != null) {
+//				list1.addAll(list2);
+//				m.put(list1.get(key), list1);
+//			}
+//		}
+//
+//		return m;
+//	}
 	
 	private static String runTime(long runT) {
 		if(runT<1000) {
