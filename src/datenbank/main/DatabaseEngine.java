59a9860225b8912d185bcc0004c776a88f8bae46
Ruoshi Li <44337694+ruoshi93@users.noreply.github.com>
2021-10-09 17:46:53 +0800

    bug in joinExample() fixed

--- src/datenbank/main/DatabaseEngine.java
+++ src/datenbank/main/DatabaseEngine.java
@@ -12,7 +12,7 @@ import datenbank.imdb.*;
 
 public class DatabaseEngine {
 
-	static Table an = new Aka_nameTable();
+//	static Table an = new Aka_nameTable();
 //	static Table at = new Aka_titleTable();;
 //	static Table ci = new Cast_infoTable();;
 //	static Table chn = new Char_nameTable();
@@ -21,13 +21,13 @@ public class DatabaseEngine {
 //	static Table ct = new Company_typeTable();
 //	static Table cc = new Complete_castTable();
 //	static Table it = new Info_typeTable();
-//	static Table k = new KeywordTable();
+	static Table k = new KeywordTable();
 //	static Table kt = new Kind_typeTable();
 //	static Table lt = new Link_typeTable();
 	static Table mc = new Movie_companiesTable();
 //	static Table mii = new Movie_info_idxTable();
 //	static Table mi = new Movie_infoTable();
-//	static Table mk = new Movie_keywordTable();
+	static Table mk = new Movie_keywordTable();
 //	static Table ml = new Movie_linkTable();
 //	static Table n = new NameTable();
 //	static Table pi = new Person_infoTable();
@@ -54,8 +54,8 @@ public class DatabaseEngine {
 //			return cc;
 //		case "it":
 //			return it;
-//		case "k":
-//			return k;
+		case "k":
+			return k;
 //		case "kt":
 //			return kt;
 //		case "lt":
@@ -66,8 +66,8 @@ public class DatabaseEngine {
 //			return mii;
 //		case "mi":
 //			return mi;
-//		case "mk":
-//			return mk;
+		case "mk":
+			return mk;
 //		case "ml":
 //			return ml;
 //		case "n":
@@ -311,7 +311,7 @@ public class DatabaseEngine {
 				} else {
 					Iterator<Map.Entry<Integer, ArrayList<Integer>>> it2 = storageResult.getData().entrySet()
 							.iterator();
-					int index2 = resultSchema.indexOf(table2);
+					int index2 = storageResult.getSchema().indexOf(table2);
 					HashMap attrPKMap1 = result.getAttrPKMap(table1, attr1);
 
 					if (attr2.equals("id")) {
@@ -518,7 +518,7 @@ public class DatabaseEngine {
 
 				Iterator<Map.Entry<Integer, ArrayList<Integer>>> it2 = resultData.entrySet().iterator();
 				Integer pk = 1;
-				
+
 				if (attr1.equals("id")) {
 					if (attr2.equals("id")) {
 						while (it2.hasNext()) {
@@ -1166,6 +1166,7 @@ public class DatabaseEngine {
 		}
 	}
 
+	// TODO
 	public static void dynamicJoinOrder(String query) {
 		Result result = new Result();
 
@@ -1175,75 +1176,96 @@ public class DatabaseEngine {
 
 	public static void main(String[] args) throws IOException {
 
-		String[] queries = { "1", "2", "3", "4", "5" };
-//		Long[] runtime = new Long[queries.length];
-		Long[] runtime = new Long[] { 0L, 0L, 0L, 0L, 0L };
+		String[] queries = { "1", "2", "3", "4", "5", "total" };
+		Long[] runtime = new Long[queries.length];
 
-//		LineChart lc = new LineChart(queries);
+		LineChart lc = new LineChart(queries);
 
 		long startTime;
 		long endTime;
 
-//		String[] orders = { "14523", "14532", "23145", "32145", "54123", "54132","23541","32541", "41523", "41532","23415","32415", "45123", "45132","23451","32451" };
-//		String[] orders = { "12", "21", "14", "41", "45", "54", "53", "35", "34", "43", "24", "42" };
+		String[] orders = { "12345", "12354", "12435", "12453", "12543", "12534", "13245", "13254", "13425", "13452",
+				"13542", "13524", "14325", "14352", "14235", "14253", "14523", "14532", "15342", "15324", "15432",
+				"15423", "15243", "15234", "21345", "21354", "21435", "21453", "21543", "21534", "23145", "23154",
+				"23415", "23451", "23541", "23514", "24315", "24351", "24135", "24153", "24513", "24531", "25341",
+				"25314", "25431", "25413", "25143", "25134", "32145", "32154", "32415", "32451", "32541", "32514",
+				"31245", "31254", "31425", "31452", "31542", "31524", "34125", "34152", "34215", "34251", "34521",
+				"34512", "35142", "35124", "35412", "35421", "35241", "35214", "42315", "42351", "42135", "42153",
+				"42513", "42531", "43215", "43251", "43125", "43152", "43512", "43521", "41325", "41352", "41235",
+				"41253", "41523", "41532", "45312", "45321", "45132", "45123", "45213", "45231", "52341", "52314",
+				"52431", "52413", "52143", "52134", "53241", "53214", "53421", "53412", "53142", "53124", "54321",
+				"54312", "54231", "54213", "54123", "54132", "51342", "51324", "51432", "51423", "51243", "51234" };
 
-//		for (String order : orders) {
+		HashMap<Integer, String> sizeArr = new HashMap<Integer, String>();
+		ArrayList<Long> runtimeArr = new ArrayList<Long>();
 
-//			System.out.println("-------------order: " + order + " --------------");
+		for (String order : orders) {
 
-		Result result = new Result();
-		for (char c : "12354".toCharArray()) {
-//			for (char c : order.toCharArray()) {
-			switch (c) {
-			case '1':
-				startTime = System.currentTimeMillis();
-				System.out.println("cn.id=mc.company_id");
-				result = join(result, "cn.id", "mc.company_id");
-				endTime = System.currentTimeMillis();
-				runtime[0] = endTime - startTime;
-				break;
-			case '2':
-				startTime = System.currentTimeMillis();
-				System.out.println("mc.movie_id=t.id");
-				result = join(result, "mc.movie_id", "t.id");
-				endTime = System.currentTimeMillis();
-				runtime[1] = endTime - startTime;
-				break;
-			case '3':
-				startTime = System.currentTimeMillis();
-				System.out.println("mk.movie_id=t.id");
-				result = join(result, "mk.movie_id", "t.id");
-				endTime = System.currentTimeMillis();
-				runtime[2] = endTime - startTime;
-				break;
-			case '4':
-				startTime = System.currentTimeMillis();
-				System.out.println("mc.movie_id=mk.movie_id");
-				result = join(result, "mc.movie_id", "mk.movie_id");
-				endTime = System.currentTimeMillis();
-				runtime[3] = endTime - startTime;
-				break;
-			case '5':
-				startTime = System.currentTimeMillis();
-				System.out.println("mk.keyword_id=k.id");
-				result = join(result, "mk.keyword_id", "k.id");
-				endTime = System.currentTimeMillis();
-				runtime[4] = endTime - startTime;
-				break;
-			default:
-				System.out.println("Error: The corresponding execution does not exist. ");
-				break;
+			System.out.println("-------------order: " + order + " --------------");
+
+			Result result = new Result();
+			for (char c : order.toCharArray()) {
+
+				switch (c) {
+				case '1':
+					startTime = System.currentTimeMillis();
+//					System.out.println("cn.id=mc.company_id");
+					result = joinExample(result, "cn.id", "mc.company_id");
+					endTime = System.currentTimeMillis();
+					runtime[0] = endTime - startTime;
+					break;
+				case '2':
+					startTime = System.currentTimeMillis();
+//				System.out.println("mc.movie_id=t.id");
+					result = joinExample(result, "mc.movie_id", "t.id");
+					endTime = System.currentTimeMillis();
+					runtime[1] = endTime - startTime;
+					break;
+				case '3':
+					startTime = System.currentTimeMillis();
+//				System.out.println("mk.movie_id=t.id");
+					result = joinExample(result, "mk.movie_id", "t.id");
+					endTime = System.currentTimeMillis();
+					runtime[2] = endTime - startTime;
+					break;
+				case '4':
+					startTime = System.currentTimeMillis();
+//					System.out.println("mc.movie_id=mk.movie_id");
+					result = joinExample(result, "mc.movie_id", "mk.movie_id");
+					endTime = System.currentTimeMillis();
+					runtime[3] = endTime - startTime;
+					break;
+				case '5':
+					startTime = System.currentTimeMillis();
+//					System.out.println("mk.keyword_id=k.id");
+					result = joinExample(result, "mk.keyword_id", "k.id");
+					endTime = System.currentTimeMillis();
+					runtime[4] = endTime - startTime;
+					break;
+				default:
+					System.out.println("Error: The corresponding execution does not exist. ");
+					break;
+				}
 			}
+			runtime[5] = runtime[0] + runtime[1] + runtime[2] + runtime[3] + runtime[4];
+			runtimeArr.add(runtime[5]);
+			printRuntimeArray(runtime);
+			System.out.println("The size of the result in the order " + order + " is: " + result.getData().size());
+			sizeArr.put(result.getData().size(), order);
+			lc.addLine(order, runtime);
+
 		}
 
-		printRuntimeArray(runtime);
-		System.out.println("The size of the result in the order 12354 is: " + result.getData().size());
-//		lc.addLine("1235", runtime);
-//		System.out.println(result);
+		
+		System.out.println("There are "+sizeArr.size()+" kind of sizes in this HashMap. ");
 
-//		}
-
-//		lc.drawLineChart();
+		Long totalRuntime = 0L;
+		for(Long l:runtimeArr) {
+			totalRuntime+=l;
+		}
+		System.out.println(totalRuntime);
+		
+		lc.drawLineChart();
 
 	}
 
